2a5da68be57372b56afae1ddf52c774328546278
RominaP <49998762+rominapao@users.noreply.github.com>
2022-10-07 22:52:53 -0300

    Add files via upload

--- proyecto/formulario.js
+++ proyecto/formulario.js
@@ -1,36 +1,36 @@
-type="text/javascript">
+
         function validar(){
         var form = document.form;
 
         if (form.nombre.value==0) {
-            alert("el campo nombre esta vacio");
+            alert("El campo nombre esta vacío");
             form.nombre.value = "";
             form.nombre.focus();
             return false;
         }
 
         if (form.edad.value == 0) {
-            alert("el campo edad esta vacio");
+            alert("El campo edad esta vacío");
             form.edad.value = "";
             form.edad.focus();
             return false;
         }
 
         if (form.correo.value == 0) {
-            alert("el campo email esta vacio");
+            alert("El campo email esta vacío");
             form.correo.value = "";
             form.correo.focus();
             return false;
         }
 
         if (form.localidad.value == 0) {
-            alert("el campo localidad esta vacio");
+            alert("El campo localidad esta vacío");
             form.localidad.value = "";
             form.localidad.focus();
             return false;
         }
 
-        alert("datos enviados con exito");
+        alert("Datos enviados con éxito");
         form.submit();
 
     }
